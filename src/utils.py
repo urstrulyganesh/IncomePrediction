80377943d3b2bb4cb9f2493477a554a2fa9a6d80
urstrulyganesh <ganeshawarde2@gmail.com>
2023-08-15 09:30:18 +0530

    ingestion

---
+++ src/utils.py
@@ -0,0 +1,48 @@
+import os
+import sys
+import pickle
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+
+from src.logger import logging
+from src.exception import CustomException
+
+
+def whitespace_remover(dataframe):
+   
+    # iterating over the columns
+    for i in dataframe.columns:
+         
+        # checking datatype of each columns
+        if dataframe[i].dtype == 'object':
+             
+            # applying strip function on column
+            dataframe[i] = dataframe[i].map(str.strip)
+        else:
+            pass
+
+    return dataframe
+
+
+def save_object(file_path,obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj,file_obj)
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
+    except Exception as e:
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
